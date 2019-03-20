0232e4da7176bab2428d64df6974269ba0a80afc
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-03-20 15:06:58 +0100

    js files changes

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -6,10 +6,14 @@ const msg = textArea.value
 const comment = document.createElement('section')
 const h3 = document.createElement('h3')
 const p = document.createElement('p')
-h3.innerHTML = '${name} said:'
+h3.innerHTML = `${name} said:`
 p.innerHTML = msg
 comment.classList.add('comment')
 comment.appendChild(h3)
 comment.appendChild(p)
 console.log(comment)
+const commentSection = document.getElementById('comments')
+commentSection.appendChild(comment)
+inputField.value = null
+textArea.value = null
 }
