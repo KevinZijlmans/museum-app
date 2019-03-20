3263aa3e2cd29eb35ed0f4680fddd2e74fb83883
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-03-20 14:50:25 +0100

    js files changes

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -6,4 +6,10 @@ const msg = textArea.value
 const comment = document.createElement('section')
 const h3 = document.createElement('h3')
 const p = document.createElement('p')
+h3.innerHTML = '${name} said:'
+p.innerHTML = msg
+comment.classList.add('comment')
+comment.appendChild(h3)
+comment.appendChild(p)
+console.log(comment)
 }
